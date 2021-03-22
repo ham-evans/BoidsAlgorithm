056eddb5dcf91fdc9637d602dd36b1e05d9a531f
Hamilton Evans <hamevans@Hamiltons-MBP.lan>
2021-03-21 22:23:15 -0700

    match velo, velo limit, bounds

--- Boids.py
+++ Boids.py
@@ -37,9 +37,89 @@ def distance (a, b):
 def nClosest (boidNum, n):
     for i in range(len(boids)):
         boids[i].distanceFrom = distance(boidNum, i)
-
     closest = sorted(boids, key=lambda boid: boid.distanceFrom)
     return closest[1:n+1]
+
+def keepWithinBounds (boidNum):
+    margin = 490
+    turnFactor = 1
+
+    if boidNum.x < margin:
+        boidNum.dx += turnFactor
+
+    if boidNum.x > width-margin:
+        boidNum.dx -= turnFactor
+
+    if boidNum.y < margin
+      boidNum.dy += turnFactor
+
+    if boidNum.y > height - margin
+      boidNum.dy -= turnFactor
+
+def flyToCenter (boidNum):
+    centeringFactor = 0.005 # Adjust velo here
+    centerX = 0
+    centerY = 0
+    numNeighbors = 0
+
+    for i in range(numBoids):
+        if i != boidNum:
+            if distance(boidNum, i) < visualRange:
+                centerX += boids[i].x
+                centerY += boids[i].y
+                numNeighbors += 1
+
+    if numNeighbors != 0:
+        centerX = centerX / numNeighbors
+        centerY = centerY / numNeighbors
+
+        boids[boidNum].dx += (centerX - boids[boidNum].x) * centeringFactor
+        boids[boidNum].dy += (centerY - boids[boidNum].y) * centeringFactor
+
+def avoidOthers (boidNum):
+    minDistance = 20
+    avoidFactor = 0.05 # Adjust velo here
+    moveX = 0
+    moveY = 0
+
+    for i in range(numBoids):
+        if i != boidNum:
+            if distance(boidNum, i) < minDistance:
+                moveX += boids[boidNum].x - boids[i].x
+                moveY += boids[boidNum].y - boids[i].y
+
+    boids[boidNum].dx += moveX * avoidFactor
+    boids[boidNum].dy += moveY * avoidFactor
+
+def matchVelocity (boidNum):
+    matchingFactor = 0.05 # adjust by % of avg velo
+
+    avgDX = 0
+    avgDY = 0
+    numNeighbors = 0
+
+    for i in range(numBoids):
+        if distance(boidNum, i) < visualRange:
+            avgDX += boids[i].dx
+            avgDY += boids[i].dy
+            numNeighbors += 1
+
+    if numNeighbors != 0:
+        avgDX = avgDX / numNeighbors
+        avgDY = avgDY / numNeighbors
+
+        boids[boidNum].dx += (avgDX - boids[boidNum].dx) * matchingFactor
+        boids[boidNum].dy += (avgDY - boids[boidNum].dy) * matchingFactor
+
+def speedLimit (boidNum):
+    speedLimit = 15
+    speed = sqrt((boids[boidNum].dx)**2 + (boids[boidNum].dy)**2)
+
+    if speed > speedLimit:
+        (boids[boidNum].dx) = (boids[boidNum].dx / speed) * speedLimit
+        (boids[boidNum].dy) = (boids[boidNum].dy / speed) * speedLimit
+
+
 
 
 
